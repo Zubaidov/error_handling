88f69939dd5804e41f7a9186ada682db7b2cf33e
Zubaidov <shohruz.zubaidov@gmail.com>
2023-09-18 00:42:20 +0400

    problem 4 upgraded

--- 2_Add_Two_Numbers.py
+++ 2_Add_Two_Numbers.py
@@ -1,23 +1,23 @@
-def addTwoNumbers(l1, l2):
-    l1 = l1[::-1]
-    l2 = l2[::-1]
-    mylists = [l1, l2]
-    final = 0
-    for mylist in mylists:
-        mynumres = 0
-        n = 1
-        for i in mylist:
-            mynum = (10**(len(mylist)-n)) * i
-            mynumres+=mynum
-            n+=1
-        final += mynumres
-    
-    final_res = [int(dig) for dig in str(final)]
-    
-    return final_res[::-1]
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        carry = 0
+        dummy = ListNode()  # Create a dummy node to simplify the code
+        current = dummy
 
+        while l1 or l2 or carry:
+            x = l1.val if l1 else 0
+            y = l2.val if l2 else 0
 
-l1 = list(map(int,input().split()))
-l2 = list(map(int,input().split()))
+            total = x + y + carry
+            carry = total // 10
+            digit = total % 10
 
-print(addTwoNumbers(l1, l2))
+            current.next = ListNode(digit)
+            current = current.next
+
+            if l1:
+                l1 = l1.next
+            if l2:
+                l2 = l2.next
+
+        return dummy.next
