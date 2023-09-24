56cb4c0ad1ad5e7d0dbd5822448a4fbbff71380b
Zubaidov <shohruz.zubaidov@gmail.com>
2023-09-24 09:24:22 +0400

    All files are renamed

--- modules/calc.py
+++
@@ -1,7 +0,0 @@
-# Any text file with the .py extension containing Python code is basically a module
-
-def sum(x,y):
-    return x+y
-
-def log(x,y):
-    return x/y
--- try_except.py
+++
@@ -1,8 +0,0 @@
-try:
-    mynum = int(input("Enter a number: "))
-    if mynum == 5:
-        print('Hello My friend')
-    else:
-        print('Hello Chuchi Kulobi')
-except ValueError:
-    print('You input the wrong format')
