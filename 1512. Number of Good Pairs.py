4becae84bd70315abd09338b2c0f6a3db9a2fe10
Zubaidov <shohruz.zubaidov@gmail.com>
2023-09-24 09:06:46 +0400

    Problem 1512

--- 1512. Number of Good Pairs.py
+++ 1512. Number of Good Pairs.py
@@ -1,3 +1,13 @@
-nums = list(map(int, input().split()))
-new_nums = nums
+from math import factorial as fact
 
+nums = list(map(int, input().split()))
+set_nums = set(nums)
+total_combinations = 0
+
+for x in set(nums):
+    tp = nums.count(x)
+    if tp>=2:
+        n = int(fact(tp)/(fact(tp-2)*fact(2)))
+        total_combinations+=n
+
+print(total_combinations)
